0602ef6beca648d71bd71b9ae07ca5b819c70a41
medusade <medusaxde@gmail.com>
2019-03-31 11:59:37 -0400

    Updated source.

--- src/talas/io/reader.hpp
+++ src/talas/io/reader.hpp
@@ -23,13 +23,115 @@
 
 #include "talas/base/base.hpp"
 #include "xos/io/reader.hpp"
+#include "xos/io/told.hpp"
 
 namespace talas {
 namespace io {
 
+typedef xos::io::from_t from_t;
+enum {
+    from_begin   = xos::io::from_begin,
+    from_end     = xos::io::from_end,
+    from_current = xos::io::from_current
+};
+
 typedef xos::io::reader reader;
 typedef xos::io::char_reader char_reader;
 typedef xos::io::byte_reader byte_reader;
+
+namespace sized {
+
+///////////////////////////////////////////////////////////////////////
+///  Class: readert
+///////////////////////////////////////////////////////////////////////
+template 
+<class TReader = reader, class TImplements = TReader, class TExtends = base>
+
+class _EXPORT_CLASS readert: virtual public TImplements, public TExtends {
+public:
+    typedef TImplements Implements;
+    typedef TExtends Extends;
+    typedef readert Derives;
+
+    typedef TReader reader_t;
+    typedef typename TImplements::what_t  what_t;
+    typedef typename TImplements::sized_t  sized_t;
+    
+    ///////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////
+    readert(const readert& copy)
+    : m_reader(copy.m_reader), m_length(copy.m_length), m_size(copy.m_size), m_tell(0) {
+    }
+    readert(reader_t& reader, ssize_t length, size_t tell = 0)
+    : m_reader(reader), m_length(length), m_size(length), m_tell(tell) {
+    }
+    virtual ~readert() {
+    }
+
+    ///////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////
+    virtual ssize_t read(what_t* what, size_t size) { 
+        if ((what) && (0 < size)) {
+            ssize_t length = 0;
+            if (0 > (length = this->length())) {
+                length = m_reader.read(what, size);
+            } else {
+                size_t tell = 0;
+                if (length < (((tell = this->tell()) + size))) {
+                    size = length - tell;
+                }
+                if (0 < (length = m_reader.read(what, size))) {
+                    this->set_tell(tell + length);
+                }
+            }
+            return length;
+        }
+        return 0; 
+    }
+
+    ///////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////
+    virtual ssize_t seek(ssize_t offset, int whence = from_begin) { 
+        return -1; 
+    }
+    virtual ssize_t set_tell(size_t to) {
+        return m_tell = to;
+    }
+    virtual ssize_t tell() const {
+        return m_tell;
+    }
+
+    ///////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////
+    virtual ssize_t set_length(ssize_t to) {
+        return m_length = to;
+    }
+    virtual ssize_t length() const {
+        return m_length;
+    }
+
+    ///////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////
+    virtual ssize_t set_size(ssize_t to) {
+        return m_size = to;
+    }
+    virtual ssize_t size() const {
+        return m_size;
+    }
+
+    ///////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////
+protected:
+    reader_t& m_reader;
+    ssize_t m_size, m_length;
+    size_t m_tell;
+}; /// class _EXPORT_CLASS readert
+
+typedef readert<io::reader> reader;
+typedef readert<io::char_reader> char_reader;
+typedef readert<io::byte_reader> byte_reader;
+
+} // namespace sized
 
 } // namespace io
 } // namespace talas 
